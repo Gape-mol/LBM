99fb3e563f298699d63db1e9c7de9cc146397924
BenjaG123 <b.garces01@ufromail.cl>
2024-09-15 20:48:44 -0300

    Se sube archivo con los test para el metodos agregarLibro.

--- src/test/java/LibraryBookManagerTest.java
+++ src/test/java/LibraryBookManagerTest.java
@@ -1,5 +1,32 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 class LibraryBookManagerTest {
+
+
+    private Object[][] biblioteca;
+
+    @BeforeEach
+    void setUp() {
+        biblioteca = new Object[100][4];
+    }
+
+    @Test
+    void testAgregarLibro() {
+        LibraryBookManager.agregarLibro(biblioteca, "12345", "Papelucho", "Marcela Paz", 3);
+        assertEquals(biblioteca[0][0], "12345");
+        assertEquals(biblioteca[0][1], "Papelucho");
+        assertEquals(biblioteca[0][2], "Marcela Paz");
+        assertEquals(biblioteca[0][3], 3);
+    }
+
+    @Test
+    void testAgregarLibroStock() {
+        LibraryBookManager.agregarLibro(biblioteca, "12345", "Papelucho", "Marcela Paz", 3);
+        LibraryBookManager.agregarLibro(biblioteca, "12345", "Papelucho", "Marcela Paz", 3);
+        assertEquals(biblioteca[0][0], "12345");
+        assertEquals(biblioteca[0][3], 6);
+    }
 
 }
