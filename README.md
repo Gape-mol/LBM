6838a7f41eab05ea2afbe1fe6f6da0d1555f1be2
Gape-mol <g.perez11@ufromail.cl>
2024-09-15 21:28:45 -0300

    Readme y comentarios en el codigo

---
+++ README.md
@@ -0,0 +1,10 @@
+### Gustavo
+
+Hice la funcion para borrar el libro, y una funcion para buscar libros mediante el ISBN, ya que asi los ubico para borrarlos.
+
+```java
+-   public static Object[][] eliminarLibro(Object[][] libros, String ISBN){}
+-   public static String leerCadena(String mensaje){}
+-   public static Object[] buscarLibro(Object[][] libros, String ISBN){}
+-   public static void menuEliminarLibro(Object[][] libros){}
+```
--- src/main/java/LibraryBookManager.java
+++ src/main/java/LibraryBookManager.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class LibraryBookManager {
     public static void main(String[] args) {
     }
-
+    // funcion que sera llamada por el menu principal para ejecutar la funcion eliminar libro
     public static void menuEliminarLibro(Object[][] libros){
         String ISBN = leerCadena("Ingrese el ISBN del libro a eliminar: ");
         eliminarLibro(libros, ISBN);
     }
-
+    //Funcion para eliminar el libro
     public static Object[][] eliminarLibro(Object[][] libros, String ISBN){
         Object[] libro = buscarLibro(libros, ISBN);
         if(libro != null){
-            Arrays.fill(libro, null);
+            Arrays.fill(libro, null); // si el libro es encontrado se llena de null ese espacio de la matriz
             System.out.println("Libro eliminado");
         }
         else{
@@ -22,8 +22,8 @@ public class LibraryBookManager {
         return libros;
     }
     public static Object[] buscarLibro(Object[][] libros, String ISBN){
-        for (Object[] libro : libros){
-            if (libro[0] == ISBN){
+        for (Object[] libro : libros){ // separo la matriz en arreglos de objetos
+            if (libro[0] == ISBN){ // y compruebo si el ISBN coincide
                 return libro;
             }
         }
