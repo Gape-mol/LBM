ec67ca94822e2f64a4d97b2ba2bce3b630fe39e5
BenjaG123 <b.garces01@ufromail.cl>
2024-10-30 20:59:58 -0300

    Se actualizan clases Usuario y Libro

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,2 +1,34 @@
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int year;
+
+    public Libro(String titulo, String autor, String isbn, int year) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.year = year;
+    }
+
+    public String getTitulo() { return titulo; }
+
+    public String getAutor() { return autor; }
+
+    public String getIsbn() { return isbn; }
+
+    public int getYear() { return year; }
+
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public void setYear(int year) { this.year = year; }
+
+    public String toString() {
+        return "Titulo: "+ this.titulo +" | Autor: "+this.autor+" | ISBN: "+this.isbn+" | Año: "+this.year;
+    }
+
 }
--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
+    private String nombre;
+    private int identificacion;
+    private List<Prestamo> prestamos;
+
+    public Usuario(String nombre, int identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        prestamos = new ArrayList<Prestamo>();
+    }
+
+    public String getNombre() { return nombre; }
+
+    public int getIdentificacion() { return identificacion; }
+
+    public List<Prestamo> getPrestamos() { return prestamos; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public void setIdentificacion(int identificacion) { this.identificacion = identificacion; }
+
+    public void setPrestamos(List<Prestamo> prestamos) { this.prestamos = prestamos; }
+
+    public void realizarPrestamo(Prestamo prestamo) { this.prestamos.add(prestamo); }
 }
