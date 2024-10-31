f15d67e9ed93c31643e3a729c7403e788c5d331f
BenjaG123 <b.garces01@ufromail.cl>
2024-10-30 21:48:29 -0300

    Se actualizan clases Usuario y Reserva

--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -5,11 +5,13 @@ public class Usuario {
     private String nombre;
     private int identificacion;
     private List<Prestamo> prestamos;
+    private List<Reserva> reservas;
 
     public Usuario(String nombre, int identificacion) {
         this.nombre = nombre;
         this.identificacion = identificacion;
         prestamos = new ArrayList<Prestamo>();
+        reservas = new ArrayList<>();
     }
 
     public String getNombre() { return nombre; }
@@ -25,4 +27,24 @@ public class Usuario {
     public void setPrestamos(List<Prestamo> prestamos) { this.prestamos = prestamos; }
 
     public void realizarPrestamo(Prestamo prestamo) { this.prestamos.add(prestamo); }
+
+    public List<Reserva> getReservas() { return reservas; }
+
+    public void setReservas(List<Reserva> reservas) { this.reservas = reservas; }
+
+    public void reservarLibro(Reserva reserva) {
+        this.reservas.add(reserva);
+        System.out.println("Reserva realizada con exito.");
+    }
+
+    public void mostrarReservas() {
+        System.out.println("Reservas de " + nombre + ":");
+        if (reservas.isEmpty()) {
+            System.out.println("No se ha reservado ningun libro.");
+        } else {
+            for (Reserva reserva : reservas) {
+                System.out.println(reserva);
+            }
+        }
+    }
 }
