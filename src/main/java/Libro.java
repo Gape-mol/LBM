9f7be88e31aced960ad46ba8aa448db6619d40bb
Gape-mol <g.perez11@ufromail.cl>
2024-11-02 16:50:11 -0300

    Añadido el atributo editorial a la clase Libro

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,2 +1,63 @@
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private String editorial;
+    private int year;
+
+    //Constructor
+    public Libro(String titulo, String autor, String ISBN, String editorial, int year) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.editorial = editorial;
+        this.year = year;
+    }
+
+    //Getters
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public String getEditorial() {
+        return this.editorial;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    //Setters
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    //To String
+    public String toString() {
+        return "Titulo: " + this.titulo + "\nAutor: " + this.autor + "\nISBN: " + this.ISBN + "\nEditorial: " + this.editorial + "\nAño: " + this.year;
+    }
 }
